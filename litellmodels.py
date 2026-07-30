98da514b469c3f6ce97c45ddcddc43c83fee2f7b
geoffroy-noel-ddh <geoffroy.noel@kcl.ac.uk>
2026-07-30 22:34:43 +0100

    fix(litellmodels): now support the new models API endpoint at ER LLM platform

--- litellmodels.py
+++ litellmodels.py
@@ -1,24 +1,44 @@
-import urllib.request
-import ssl
 import json
+import ssl
+import urllib.request
+
 from env.litellmodels import API_URL, TOKEN
 
-ssl_context = ssl._create_unverified_context()
+IS_ER_API = True
 
-url = f"{API_URL}/model/info"
-headers = {
-    'User-Agent': 'Mozilla/5.0',
-    'Authorization': f'Bearer {TOKEN}'
-}
-request = urllib.request.Request(url, headers=headers)
-response = urllib.request.urlopen(request, context=ssl_context)
-res_str  = response.read().decode('utf-8')
-res_dic = json.loads(res_str)
+def call_json_api(url, token, print_json=False):
+    ssl_context = None
+    if 'localhost' in url:
+        ssl_context = ssl._create_unverified_context()
 
-#print(json.dumps(res_dic, indent=2))   
+    headers = {
+        'User-Agent': 'Mozilla/5.0',
+        'Authorization': f'Bearer {token}'
+    }
+    print(f'REQUEST {url}')
+    request = urllib.request.Request(url, headers=headers)
+    response = urllib.request.urlopen(request, context=ssl_context)
+    res_str  = response.read().decode('utf-8')
+    ret = json.loads(res_str)
 
-for model in res_dic['data']:
-    params = model["litellm_params"]
-    info = model["model_info"]
-    vision_status = 'VISION' if info["supports_vision"] else ''
-    print(f'{model["model_name"]:<15} {params["model"]:<30} {info["backend_model"]:<25} {str(info["max_tokens"]):>7} {vision_status:<6}')
+    if print_json:
+        print(json.dumps(ret, indent=2))
+
+    return ret
+
+if IS_ER_API:
+    # openai/er api
+    res_dic = call_json_api(f"{API_URL}/api/v1/models", TOKEN)
+
+    for model in res_dic:
+        vision_status = 'VISION' if model["supports_vision"] else ''
+        print(f'{model["name"]:<15} {model["provider"]:<20} {model["backend_model"]:<30} {str(int(model["context_window"]/1024)):>6}k {vision_status:<6}')
+else:
+    # litellm api (used to be suuported by ER LLM platform)
+    res_dic = call_json_api(f"{API_URL}/model/info", TOKEN)
+
+    for model in res_dic['data']:
+        params = model["litellm_params"]
+        info = model["model_info"]
+        vision_status = 'VISION' if info["supports_vision"] else ''
+        print(f'{model["model_name"]:<15} {params["model"]:<30} {info["backend_model"]:<25} {str(info["max_tokens"]):>7} {vision_status:<6}')
